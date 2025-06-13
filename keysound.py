0490d7be696dc75780c690de62ca48bfca537e3d
emiHuy <emilyhuynh06@gmail.com>
2025-06-13 17:32:47 -0400

    feat: add mario and animal crossing themes.

--- keysound.py
+++ keysound.py
@@ -3,9 +3,7 @@ import pygame
 import random
 
 def key_pressed(key):
-    minecraft_theme(key)
-    # pygame.mixer.music.load("assets/sounds/pop.mp3")
-    # pygame.mixer.music.play(loops=0)
+    animal_crossing_theme(key)
 
 def play_sound(path, volume=1):
     sound = pygame.mixer.Sound(path)
@@ -32,9 +30,42 @@ def minecraft_theme(key):
             play_sound("assets/sounds/minecraft/grass/grass" + str(variation) + ".wav", 0.6)
     except:
         play_sound("assets/sounds/minecraft/pop.wav", 0.6)
-    
-if __name__ == "__main__":
+
+def mario_theme(key):
+    try:
+        if key == keyboard.Key.enter:
+            play_sound("assets/sounds/mario/pipe.wav")
+        elif key == keyboard.Key.shift:
+            pass
+        elif key == keyboard.Key.space:
+            play_sound("assets/sounds/mario/jump.wav", 0.9)
+        elif key.char.isalpha():
+            play_sound("assets/sounds/mario/fireball.wav", 0.8)
+        elif key.char.isnumeric():
+            play_sound("assets/sounds/mario/bump.wav", 0.8)
+        else:
+            play_sound("assets/sounds/mario/bump.wav", 0.8)
+    except:
+         play_sound("assets/sounds/mario/coin.wav", 0.5)
+
+def animal_crossing_theme(key):
+    try:
+        if key == keyboard.Key.enter:
+            play_sound("assets/sounds/animal-crossing/confirm.mp3", 0.3)
+        elif key == keyboard.Key.shift:
+            pass
+        elif key == keyboard.Key.space or key.char.isalpha() or key.char.isnumeric():
+            play_sound("assets/sounds/animal-crossing/intense.mp3", 1.4)
+        else:
+            play_sound("assets/sounds/animal-crossing/greetings.mp3", 1.2)
+    except:
+         play_sound("assets/sounds/animal-crossing/decline.mp3", 0.8)
+
+def run():
     pygame.mixer.init()
     listener = keyboard.Listener(on_press=key_pressed)
     listener.start()
-    input()
+    input() 
+
+if __name__ == "__main__":
+    run()
